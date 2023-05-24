90433d8d87f94ff8aab451a15cfb574c8b39d801
eman0202byu <eman0202@byu.edu>
2023-05-23 20:01:13 -0500

    SpaceBar.sh(Implemented)

---
+++ Store.js
@@ -0,0 +1,11 @@
+let spaceCore = {
+    hat: 0,
+    necklace: 0,
+    lHand: 0,
+    rHand: 0,
+}
+// Each image will be assigned a letter and number
+// h === hat; n === necklace; l === lHand; r === rHand;
+// The numbers will be in the order the image is added to the database
+// 0 === NONE;
+// EX: partyhat.jpg === h1;
