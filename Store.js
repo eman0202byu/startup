3dffb89196b1ff5e3e58d68ee574a1f53ebec053
eman0202byu <eman0202@byu.edu>
2023-05-24 20:18:20 -0500

    Store.js(Fixed.Daily Items)

--- Store.js
+++ Store.js
@@ -75,8 +75,8 @@ const fetchStore = new Promise((resolve,reject) => {
     if(fetchSuccess === true){
         if(outsideStore == 'NONE'){
         Store = {
-           item1: ['imgID1', '100'],
-           item2: ['imgID2', '100'],
+           item1: ['partyhat.jpg', '100'],
+           item2: ['plainnecklace.png', '100'],
            item3: ['imgID3', '100'],
            item4: ['imgID4', '100'],
            item5: ['imgID5', '100'],
@@ -85,12 +85,6 @@ const fetchStore = new Promise((resolve,reject) => {
            item8: ['imgID8', '100'],
            item9: ['imgID9', '100'],
         }
-        // Each image will be assigned a letter and number
-        // h === hat; n === necklace; l === lHand; r === rHand;
-        // The numbers will be in the order the image is added to the database
-        // 0 === NONE;
-        // EX: partyhat.jpg === h1;
-        // This will be packaged in to a .json and send to the DB and then the DB will the user's SpaceCore.png reference.
         localStorage.setItem('Store', JSON.stringify(Store)); //Update with database push
         } else {
             Store = JSON.parse(outsideStore);
@@ -106,6 +100,20 @@ fetchStore
     .catch((ERRORCODE) => console.error(`Error: ${ERRORCODE}`))
     .finally(() => console.log('Log: fetchStore finished'));
 
+
+function storeUP(){
+    for (let buttonNumber = 1; buttonNumber < 10; buttonNumber++) {
+        const buttonID = '#item' + buttonNumber;
+        const buttonDOM = document.querySelector(buttonID);
+        const key = 'item' + buttonNumber;
+        let item = Store[key];
+        let itemImage, itemValue = null;
+        [itemImage, itemValue] = item;
+
+        buttonDOM.innerHTML = `<td id="item${buttonNumber}"><div><div id="picture" class="picture-box"><img width="50px" src="${itemImage}" alt="random" /></div></div><div>§${itemValue}</div><button type="" onclick="Bpress(${buttonNumber})">Buy</button></td>`;
+    }
+}
+storeUP();
 
 function Bpress(buttonNumber){
     const buttonID = '#item' + buttonNumber;
