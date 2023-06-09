b11be881e24c2699aeb34f48b0e601358c1d0be6
eman0202byu <eman0202@byu.edu>
2023-06-08 20:06:18 -0500

    Websockets(implemented)

--- public/SpaceBar.js
+++ public/SpaceBar.js
@@ -1,3 +1,6 @@
+let socket = null;
+configureWebSocket();
+
 const user = localStorage.getItem('userName') ?? 404;
 if((user == 404)){
     window.location.href = "index.html";
@@ -122,6 +125,7 @@ function pullImage(){
 pullImage();
 
 function cheating(){
+    broadcastEvent(userObj.name, 'message', (JSON.stringify(userObj)));
     document.body.innerHTML =
     `<body id="caught"><header><h1>You Clicked too fast</h1><li><span class="text-reset">Your account has been suspended by the spacecops contact the admin to review your case.</span><br /><a href="https://github.com/eman0202byu/startup">GitHub</a></li></header></body>`;
 }
@@ -156,8 +160,44 @@ function cheatCheck(){
         currencychk = currency;
     }
 }
-cheatCheck();
 
 let cheatChecker = cheatCheck;
 
 let intervalID = setInterval(cheatChecker, 5000);
+
+/////////////////////////////////////////////////////
+
+async function configureWebSocket() {
+    const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+    socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+    socket.onopen = (event) => {
+      console.log('SpaceBar WS connected');
+    };
+    socket.onclose = (event) => {
+      console.log('SpaceBar WS disconnected');
+    };
+    socket.onmessage = async (event) => {
+        try {
+            const msg = JSON.parse(await event.data.text());
+            if (msg.name != null) {
+                alert(`${msg.name} has been caught by the Space Cops! (They were cheating)`);
+          } else {
+                alert("A player has been caught by the Space Cops! (They were cheating)");
+          }
+        } catch (e){
+            alert(`${msg}`);
+        }
+    };
+  }
+
+  async function broadcastEvent(from, type, value) {
+    if(socket == null){
+        await configureWebSocket();
+    }
+    const event = {
+      from: from,
+      type: type,
+      value: value,
+    };
+    socket.send(JSON.stringify(event));
+  }
