57100d790265f3be4fc405dde02ec6150303c51b
eman0202byu <eman0202@byu.edu>
2023-06-15 12:49:29 -0500

    Finished

--- src/app.jsx
+++ src/app.jsx
@@ -5,41 +5,47 @@ import { Login } from './login/login';
 import { SpaceBar } from './spacebar/SpaceBar';
 
 function App() {
-    return (
-      <BrowserRouter>
-        <div>
-          <header>
-            <nav>
-              <div className='navbar-brand'>
+  return (
+    <BrowserRouter>
+      <div>
+        <header>
+          <nav>
+            <div className='navbar-brand'>
               Space Salary
-              </div>
-              <menu className='navbar-nav'>
-                <li className='nav-item'>
-                  <NavLink className='nav-link' to=''>
-                    Login
-                  </NavLink>
-                </li>
-                  <li className='nav-item'>
-                    <NavLink className='nav-link' to=''>
-                      Spacebar
-                    </NavLink>
-                  </li>
-              </menu>
-            </nav>
-          </header>
-  
-          <Routes>
-            <Route path='/' element={<Login />} exact />
-            <Route path='/spacebar' element={<SpaceBar/>} />
-            <Route path='*' element={<NotFound />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    );
-  }
+            </div>
+            <menu className='navbar-nav'>
+              <li className='nav-item'>
+                <NavLink className='nav-link' to=''>
+                  Login
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink className='nav-link' to=''>
+                  Spacebar
+                </NavLink>
+              </li>
+            </menu>
+          </nav>
+        </header>
 
-  function NotFound() {
-    return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
-  }
+        <Routes>
+          <Route path='/' element={<Login />} exact />
+          <Route path='/spacebar' element={<SpaceBar />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </div>
 
-  export default App;
+      <footer>
+        <span>Made by: Ethan Richmond</span>
+        <a href="https://github.com/eman0202byu/startup">GitHub</a>
+      </footer>
+
+    </BrowserRouter>
+  );
+}
+
+function NotFound() {
+  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
+}
+
+export default App;
